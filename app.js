63974e1d98e13bcf6c807e97b4ae2448dc5c9d24
azam848 <armankhan80808076@gmail.com>
2025-08-06 18:00:32 +0530

    clean push

---
+++ app.js
@@ -0,0 +1,8 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "hello world from react - by arman khan!!"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
