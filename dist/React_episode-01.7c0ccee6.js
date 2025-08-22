b45fe3f36750a2d75a5243af531095d7aa3d4ec6
azam848 <armankhan80808076@gmail.com>
2025-08-22 18:39:09 +0530

    Clean repo by removing cache, dist and node_modules

--- dist/React_episode-01.7c0ccee6.js
+++
@@ -1,7 +0,0 @@
-const heading = React.createElement("h1", {
-    id: "heading"
-}, "hello world from react - by arman khan!!");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
-
-//# sourceMappingURL=React_episode-01.7c0ccee6.js.map
